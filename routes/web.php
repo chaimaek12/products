93f901341a0a17950b72dac01469505aa6ad8797
chaimaek12 <exo@gmail.com>
2026-03-04 17:49:11 +0100

    route

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products.index');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/c', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
